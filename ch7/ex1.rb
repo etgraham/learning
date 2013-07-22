0eeb33c8f943318760d26a4c9f59ffe7245aed03
Emily Graham <etgraham@containerstore.com>
2013-07-22 12:29:59 -0500

    updating ch7 ex1 again

--- ch7/ex1.rb
+++ ch7/ex1.rb
@@ -1,11 +1,15 @@
 var = 99
 var2 = var-1
 while var > 0
+bottle = "bottles"
+if var == 1
+  bottle = "bottle"
+end
 puts ""
-puts "#{var} bottles of beer on the wall, #{var} bottles of beer."
+puts "#{var} bottles of beer on the wall, #{var} #{bottle} of beer."
 var = var - 1
-puts "Take one one down and pass it around, #{var} bottles of beer on the wall."
+puts "Take one one down and pass it around, #{var} #{bottle} of beer on the wall."
 end
 puts ""
 puts "No more bottles of beer on the wall, no more bottles of beer."
-puts "Go to the store and buy some more, #{var} bottles of beer on the wall."
+puts "Go to the store and buy some more, 99 bottles of beer on the wall."
