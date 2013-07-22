cd8d7048fdba0169abac4eb413ec656b554894cc
Emily Graham <etgraham@containerstore.com>
2013-07-22 12:18:28 -0500

    updating ch7 ex 1

---
+++ ch7/ex1.rb
@@ -0,0 +1,11 @@
+var = 99
+var2 = var-1
+while var > 0
+puts ""
+puts "#{var} bottles of beer on the wall, #{var} bottles of beer."
+var = var - 1
+puts "Take one one down and pass it around, #{var} bottles of beer on the wall."
+end
+puts ""
+puts "No more bottles of beer on the wall, no more bottles of beer."
+puts "Go to the store and buy some more, #{var} bottles of beer on the wall."
